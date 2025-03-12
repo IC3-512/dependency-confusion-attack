445ff432f0d72f2b531983699187322c7c35f142
Entleutner Max <max.entleutner@gmail.com>
2025-03-12 05:50:32 -0400

    init

---
+++ package/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package_346234293',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies your package needs
+    ]
+)
